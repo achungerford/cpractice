aab100c1992962c5ae93c94ffe5e75d571bde577
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-05-05 20:41:06 -0700

    a C program's structure

---
+++ 00_structure.c
@@ -0,0 +1,25 @@
+/*
+	The layout of a C program.
+
+	author: Alexander Hungerford
+
+	Lesson:
+		comments: single-line vs. multi-line
+
+		function definition vs. function declaration
+
+		If you define your functions before they are called -- above main(),
+		then you aren't required to have a function declaration.
+*/
+
+#include <stdio.h>
+
+// function declarations or definitions here
+
+int main()
+{
+	// your program here
+	return 0;
+}
+
+// function definitions here (requires declaration above)
